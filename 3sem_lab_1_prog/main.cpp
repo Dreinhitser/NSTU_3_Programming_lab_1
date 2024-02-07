79ef97cc3450f99985b6fe810575f4070eef547f
Denis Rikhter <48100891+Dreinhitser@users.noreply.github.com>
2024-02-07 16:22:54 +0700

    fix codestyle

--- 3sem_lab_1_prog/main.cpp
+++ 3sem_lab_1_prog/main.cpp
@@ -3,53 +3,46 @@
 #include <string>
 #include <ctime>
 
-using namespace std;
 
 // randomly selects a character from the input file name
-char generate_byte_encoding(const string& text)
+char generate_byte_encoding(const std::string& text)
 {
 	srand(time(nullptr));
-
 	return text[rand() % text.size()];
 }
 
 // writes the string line to the binary file
-void write_bin_file(const string& filename, const string& line, const char byte_encoding)
+void write_bin_file(const std::string& filename, const std::string& line, const char byte_encoding)
 {
-	ofstream out(filename, ios::binary | ios::app);
-	if (!out.is_open())
-	{
-		throw exception("Failed to open the output binary file");
+	std::ofstream out(filename, std::ios::binary | std::ios::app);
+	if (!out.is_open()) {
+		throw std::exception("Failed to open the output binary file");
 	}
 
 	char size = line.size();
 
-	out.write((char*)&size, sizeof(size));
-	for (int i = 0; i < line.size(); i++)
-	{
+	out.write(&size, sizeof(size));
+	for (int i = 0; i < line.size(); i++) {
 		char symbol = line[i] ^ byte_encoding;
-		out.write((char*)&symbol, sizeof(symbol));
+		out.write(&symbol, sizeof(symbol));
 	}
 
 	out.close();
 }
 
 // returns the contents of a binary file as a string
-string read_bin_file(const string& filename, const char byte_encoding)
+std::string read_bin_file(const std::string& filename, const char byte_encoding)
 {
-	ifstream in(filename, ios::binary);
-	if (!in.is_open())
-	{
-		throw exception("Failed to open the input binary file");
+	std::ifstream in(filename, std::ios::binary);
+	if (!in.is_open()) {
+		throw std::exception("Failed to open the input binary file");
 	}
 
-	string received_text = "";
+	std::string received_text = "";
 	char size = 0;
-	while (in.read(&size, sizeof(size)))
-	{
-		for (int i = 0; i < size; i++)
-		{
-			char c;
+	while (in.read(&size, sizeof(size))) {
+		for (int i = 0; i < size; i++) {
+			char c = 0;
 
 			in.read(&c, sizeof(c));
 			c ^= byte_encoding;
@@ -64,43 +57,37 @@ string read_bin_file(const string& filename, const char byte_encoding)
 
 int main(int argc, char* argv[])
 {
-	const string filename_in = string(argv[1]);
-	const string filename_out = string(argv[1]) + ".bin";
+	const std::string filename_in = std::string(argv[1]);
+	const std::string filename_out = std::string(argv[1]) + ".bin";
 
-	ifstream in(filename_in);
+	std::ifstream in(filename_in);
 
-	if (!in.is_open())
-	{
-		cerr << "Failed to open the input file\n";
+	if (!in.is_open()) {
+		std::cerr << "Failed to open the input file\n";
 		return 1;
 	}
 	
 	// reading from a .txt file and writing to .bin file
 	char byte_encoding = generate_byte_encoding(filename_in);
 
-	string line = "";
-	while (getline(in, line))
-	{
-		try
-		{
+	std::string line = "";
+	while (std::getline(in, line)) {
+		try {
 			write_bin_file(filename_out, line, byte_encoding);
 		}
-		catch (exception ex)
-		{
-			cerr << ex.what() << "\n";
+		catch (std::exception ex) {
+			std::cerr << ex.what() << "\n";
 			return 1;
 		}
 	}
 	in.close();
 
 	// reading from .bin file and content output to the console
-	try
-	{
-		cout << read_bin_file(filename_out, byte_encoding);
+	try {
+		std::cout << read_bin_file(filename_out, byte_encoding);
 	}
-	catch (exception ex)
-	{
-		cerr << ex.what() << "\n";
+	catch (std::exception ex) {
+		std::cerr << ex.what() << "\n";
 		return 1;
 	}
 
